aa9335b91f52309dccafd6f286e474e1fc8de660
azukaegoo <azumgloria@gmail.com>
2023-07-20 03:49:23 -0700

    commit for printing a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTERS_HEADER
 #define FUNCTION_POINTERS_HEADER
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
